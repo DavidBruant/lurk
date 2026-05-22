46f155d36901bad1edbd2fa33baecb7c688ae602
David Bruant <davidbruant@protonmail.com>
2026-05-22 10:40:29 +0200

    Tests with nextest

--- tests/basic.rs
+++ tests/basic.rs
@@ -12,14 +12,15 @@ fn bad_add(a: i32, b: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use std::io::{Write};
-
+    use std::io::{self, IsTerminal, Write};
+    use syscalls::Sysno;
+    
     use anyhow::{Error, Result, bail};
     use nix::unistd::{fork, ForkResult};
-
+    
+    use lurk_cli::style::StyleConfig;
     use lurk_cli::args::{ArgCommand, Args};
     use lurk_cli::{run_tracee, Tracer};
-    use syscalls::Sysno;
 
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     //use super::*;
@@ -43,6 +44,8 @@ mod tests {
     fn lurk_tracer_ls() -> Result<(), Error> {
         let command = [String::from("ls")];
 
+        println!("TEST lurk_tracer_ls");
+
         // create Trace instance manually
         // fed it "ls"
         let config= Args::from({Args { 
@@ -62,10 +65,10 @@ mod tests {
             expr: Vec::new(), 
             json: false, 
             collapse_exec_retries: false, 
-            command: Some(ArgCommand::Command(vec!["ls".to_string()])),
+            command: Some(ArgCommand::Command(vec![])),
         }});
 
-        let pid = {
+        let child_pid = {
             match unsafe { fork() } {
                 Ok(ForkResult::Child) => return run_tracee(&command, &config.env, &None),
                 Ok(ForkResult::Parent { child }) => child,
@@ -75,16 +78,85 @@ mod tests {
 
         let output: Box<dyn Write> = Box::new(std::io::stdout());
 
-        let mut tracer = Tracer::new(pid, config, output)?;
+        println!("TEST lurk_tracer_ls - tracer.run_tracer");
+
+        let mut tracer = Tracer::new(child_pid, config, output)?;
+        let _ = tracer.run_tracer();
+
+        println!("TEST lurk_tracer_ls - after tracer.run_tracer");
+
+        // get tracer.syscall_infos.
+        let syscalls = tracer.syscall_infos;
+
+        println!("{} syscalls", syscalls.len());
+
+        // perform filters to find the 'fstat'
+        let fstat_syscalls: Vec<&lurk_cli::syscall_info::SyscallInfo> = syscalls.iter().filter(|&si| si.syscall == Sysno::fstat).collect();
+
+        println!("{} syscalls fstat", fstat_syscalls.len());
+
+        assert!(fstat_syscalls.len() >= 1, "At least one call to fstat during call to ls");
+
+        Ok(())
+    }
+
+    
+    #[test]
+    fn exec_tracer_cat() -> Result<(), Error> {
+        let command = [String::from("cat"), String::from(".gitignore")];
+
+        let mut style_config = StyleConfig::default();
+
+        style_config.use_colors = io::stdout().is_terminal();
+
+
+        let config= Args::from({Args { 
+            syscall_number: false, 
+            attach: None, 
+            no_abbrev: false, 
+            string_limit: None, 
+            file: None, 
+            summary_only: false, 
+            summary: false, 
+            successful_only: false, 
+            failed_only: false, 
+            env: Vec::new(), 
+            username: None, 
+            follow_forks: true, 
+            syscall_times: false, 
+            expr: Vec::new(), 
+            json: false, 
+            collapse_exec_retries: false,
+            command: Some(ArgCommand::Command(vec![])),
+        }});
+
+        let child_pid = {
+            match unsafe { fork() } {
+                Ok(ForkResult::Child) => return run_tracee(&command, &config.env, &None),
+                Ok(ForkResult::Parent { child }) => child,
+                Err(err) => bail!("fork() failed: {err}"),
+            }
+        };
+
+        let output: Box<dyn Write> = Box::new(std::io::stdout());
+
+        let mut tracer = Tracer::new(child_pid, config, output)?;
         let _ = tracer.run_tracer();
 
         // get tracer.syscall_infos.
         let syscalls = tracer.syscall_infos;
 
         // perform filters to find the 'fstat'
-        let fstat_syscalls: Vec<&lurk_cli::syscall_info::SyscallInfo> = syscalls.iter().filter(|&si| si.syscall == Sysno::fstat).collect();
+        let openat_syscalls: Vec<&lurk_cli::syscall_info::SyscallInfo> = syscalls.iter().filter(|&si| si.syscall == Sysno::openat).collect();
 
-        assert!(fstat_syscalls.len() >= 1, "At least one call to fstat during call to ls");
+        assert!(openat_syscalls.len() >= 1, "At least one call to openat during call to ls");
+
+
+        let mut output: Box<dyn Write> = Box::new(std::io::stdout());
+
+        for syscall in openat_syscalls {
+            let _ = syscall.write_syscall(style_config.clone(), None, true, false, &mut output);
+        }
 
         Ok(())
     }
